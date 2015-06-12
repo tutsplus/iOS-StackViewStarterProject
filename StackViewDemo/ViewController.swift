7e7f8f03049ae908f44b457f417f35266a202b63
Jordan Morgan <info@dreaminginbinary.co>
2015-06-12 17:49:03 -0500

    Finished project.

--- StackViewDemo/ViewController.swift
+++ StackViewDemo/ViewController.swift
@@ -13,17 +13,29 @@ class ViewController: UIViewController
     
     @IBOutlet weak var verticalStackView: UIStackView!
     @IBOutlet weak var horizontalStackView: UIStackView!
-    override func viewDidLoad()
+    
+    @IBAction func removeStar(sender: AnyObject)
     {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning()
-    {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let star:UIView? = self.horizontalStackView.arrangedSubviews.last
+        if let aStar = star
+        {
+            self.horizontalStackView.removeArrangedSubview(aStar)
+            aStar.removeFromSuperview()
+            UIView.animateWithDuration(0.25, animations: {
+                self.horizontalStackView.layoutIfNeeded()
+            })
+        }
     }
     
+    @IBAction func addStar(sender: AnyObject)
+    {
+        let starImgVw:UIImageView = UIImageView(image: UIImage(named: "star"))
+        starImgVw.contentMode = .ScaleAspectFit
+        self.horizontalStackView.addArrangedSubview(starImgVw)
+        UIView.animateWithDuration(0.25, animations: {
+            self.horizontalStackView.layoutIfNeeded()
+        })
+        
+    }
 }
 
