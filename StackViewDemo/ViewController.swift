965dbec8506de9b34485efe816ec975b7b873ff3
Jordan Morgan <info@dreaminginbinary.co>
2015-06-12 16:20:45 -0500

    UIStackView Initial commit.
    Starting point for the tutorial.

---
+++ StackViewDemo/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by Jordan Morgan on 6/10/15.
+//  Copyright © 2015 Jordan Morgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    
+    @IBOutlet weak var verticalStackView: UIStackView!
+    @IBOutlet weak var horizontalStackView: UIStackView!
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    
+}
+
